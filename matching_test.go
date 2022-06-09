ed363980b1c344a718cf5263ecf096ab021257aa
yutohub <83153839+yutohub@users.noreply.github.com>
2022-06-09 22:18:59 +0900

    Fix test

--- matching_test.go
+++ matching_test.go
@@ -1,12 +1,13 @@
 package matching_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/yutohub/matching"
 )
 
-func TestGaleShapley4x4(t *testing.T) {
+func TestGS4x4(t *testing.T) {
 	msPrefers := [][]int{
 		{0, 1, 2, 3},
 		{3, 0, 1, 2},
@@ -20,7 +21,11 @@ func TestGaleShapley4x4(t *testing.T) {
 		{3, 2, 0, 1},
 	}
 	answer := map[int]int{0: 0, 1: 2, 2: 3, 3: 1}
-	result := matching.GaleShapley(msPrefers, wsPrefers)
+	result, err := matching.GaleShapley(msPrefers, wsPrefers)
+	matching.Show(result)
+	if err != nil {
+		t.Error(err)
+	}
 	for i := range answer {
 		if answer[i] != result[i] {
 			t.Errorf("ERROR: m%v <-> w%v is not collect", result[i], i)
@@ -28,7 +33,7 @@ func TestGaleShapley4x4(t *testing.T) {
 	}
 }
 
-func TestGaleShapley3x3(t *testing.T) {
+func TestGS3x3(t *testing.T) {
 	msPrefers := [][]int{
 		{2, 3, 1},
 		{2, 1, 3},
@@ -42,7 +47,11 @@ func TestGaleShapley3x3(t *testing.T) {
 		{0, 1, 2},
 	}
 	answer := map[int]int{0: 3, 2: 1, 3: 0}
-	result := matching.GaleShapley(msPrefers, wsPrefers)
+	result, err := matching.GaleShapley(msPrefers, wsPrefers)
+	matching.Show(result)
+	if err != nil {
+		t.Error(err)
+	}
 	for i := range answer {
 		if answer[i] != result[i] {
 			t.Errorf("ERROR: m%v <-> w%v is not collect", result[i], i)
@@ -50,46 +59,28 @@ func TestGaleShapley3x3(t *testing.T) {
 	}
 }
 
-func TestGaleShapley2x2(t *testing.T) {
-	msPrefers := [][]int{
-		{2, 3},
-		{2, 1},
-		{0, 3},
-		{0, 2},
+func TestGSLessMsPrefers(t *testing.T) {
+	msPrefers := [][]int{}
+	wsPrefers := [][]int{{0}, {1}, {3}, {2}}
+	result, err := matching.GaleShapley(msPrefers, wsPrefers)
+	fmt.Printf("TestGSLessMsPrefers: %s\n", err)
+	if result != nil {
+		t.Error("ERROR: Expect an error and no result returned")
 	}
-	wsPrefers := [][]int{
-		{1, 0},
-		{3, 0},
-		{1, 3},
-		{0, 2},
-	}
-	answer := map[int]int{2: 1, 3: 0}
-	result := matching.GaleShapley(msPrefers, wsPrefers)
-	for i := range answer {
-		if answer[i] != result[i] {
-			t.Errorf("ERROR: m%v <-> w%v is not collect", result[i], i)
-		}
+	if err == nil {
+		t.Error("ERROR: Expect error occurred")
 	}
 }
 
-func TestGaleShapley1x1(t *testing.T) {
-	msPrefers := [][]int{
-		{0},
-		{1},
-		{3},
-		{0},
+func TestGSLessWsPrefers(t *testing.T) {
+	msPrefers := [][]int{{0}, {1}, {3}, {2}}
+	wsPrefers := [][]int{}
+	result, err := matching.GaleShapley(msPrefers, wsPrefers)
+	fmt.Printf("TestGSLessWsPrefers: %s\n", err)
+	if result != nil {
+		t.Error("ERROR: Expect an error and no result returned")
 	}
-	wsPrefers := [][]int{
-		{0},
-		{1},
-		{3},
-		{2},
-	}
-	answer := map[int]int{0: 0, 1: 1, 3: 2}
-	result := matching.GaleShapley(msPrefers, wsPrefers)
-	for i := range answer {
-		if answer[i] != result[i] {
-			t.Errorf("ERROR: m%v <-> w%v is not collect", result[i], i)
-		}
+	if err == nil {
+		t.Error("ERROR: Expect error occurred")
 	}
 }
